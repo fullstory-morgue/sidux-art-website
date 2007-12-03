89bb9331a45173ae62f178a562ac0656545fe266
Chris Hildebrandt <slam@aptosid.com>
2007-12-03 13:00:28 +0000

    add eros web site theme, container and page width fixes included

---
+++ eros-theme/html/themes/eros/lang/fra/xaninit.php
@@ -0,0 +1,28 @@
+<?php
+// ----------------------------------------------------------------------
+// Original Author of file: Everyone
+// Purpose of file: Translation files
+// Translation team: Read credits in /docs/CREDITS.txt
+// ----------------------------------------------------------------------
+
+// Theme Settings
+define('_TM_PAGEWIDTH','Overall page width');
+define('_TM_LCOLWIDTH','Left column width');
+define('_TM_RCOLWIDTH','Right column width');
+define('_TM_INDEXCOL','Number of news columns');
+define('_TM_RIGHTON','Show right blocks in news module (only applicable for PostNuke .7x)');
+
+// Theme Zones
+define('_TM_MASTER','Master zone');
+define('_TM_OPENTABLE1','OpenTable1');
+define('_TM_OPENTABLE2','OpenTable2');
+define('_TM_LEFTSIDEB','Left side box');
+define('_TM_RIGHTSIDEB','Right side box');
+define('_TM_NEWSINDEX','News index (standard)');
+define('_TM_NEWSART','News article');
+define('_TM_NEWSINDEX2','News index (2 columns)');
+define('_TM_MAINMENUB','Main menu block');
+define('_TM_CENTERB','Center block');
+define('_TM_HOMEPAGE', 'Homepage');
+
+?>
---
+++ eros-theme/html/themes/eros/xaninfo.php
@@ -0,0 +1,13 @@
+<?php
+// ----------------------------------------------------------------------
+// Original Author of file:  Mark West
+// Purpose of file: Xanthia theme information
+// ----------------------------------------------------------------------
+
+$themeinfo['name'] = 'eros';
+$themeinfo['author'] = 'Chris Hildebrandt';
+$themeinfo['download'] = 'http://sidux.com';
+$themeinfo['api'] = 'Xanthia2.0';
+$themeinfo['xhtmlsupport'] = 1;
+
+?>
