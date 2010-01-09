d84ecd75b0291d06ae07f16c66f8dec0ccb5c7fa
Chris Hildebrandt <slam@aptosid.com>
2010-01-09 09:24:02 +0000

    fix default logo and preview image, add ypnos theme

---
+++ ypnos-theme/themes/ypnos/version.php
@@ -0,0 +1,16 @@
+<?php
+$themeversion['name'] = ypnos;
+$themeversion['displayname'] = ypnos;
+$themeversion['version'] = '1.0';
+$themeversion['description'] = 'ypnos theme for sidux.com';
+$themeversion['author'] = 'Chris (slam) Hildebrandt';
+$themeversion['contact'] = 'http://sidux.com/';
+$themeversion['admin'] = false;
+$themeversion['user'] = true;
+$themeversion['system'] = false;
+$themeversion['credits'] = '';
+$themeversion['help'] = '';
+$themeversion['changelog'] = '';
+$themeversion['license'] = '';
+$themeversion['xhtml'] = true;
+?>
